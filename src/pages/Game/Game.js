db783a720b8cb3ee4d7ecf9ffacf81fb8afe3c03
Joseph <ejosephvito@gmail.com>
2020-02-21 20:13:47 -0500

    getting clicking to work

--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -11,23 +11,39 @@ import "./Game.css";
 import randBoolean from "./scripts/randBoolean.js";
 
 class Game extends Component {
-  state = {
-    activePlayer: "Test",
-    grid: {
-      cell_1: "",
-      cell_2: "",
-      cell_3: "",
-      cell_4: "",
-      cell_5: "",
-      cell_6: "",
-      cell_7: "",
-      cell_8: "",
-      cell_9: ""
-    }
-  };
+  constructor() {
+    super();
+
+    this.state = {
+      activePlayer: null,
+      grid: {
+        cell_1: null,
+        cell_2: null,
+        cell_3: null,
+        cell_4: null,
+        cell_5: null,
+        cell_6: null,
+        cell_7: null,
+        cell_8: null,
+        cell_9: null
+      }
+    };
+  }
 
   componentDidMount() {
-    this.setState({ activePlayer: randBoolean() ? "human" : "ai" }, () => {});
+    this.setState({ activePlayer: randBoolean() ? "human" : "ai" }, () => console.log(this.state.activePlayer));
+  }
+
+  cellClicked(cellID) {
+    console.log("cellClicked");
+
+    if (this.state.activePlayer !== "human") {
+      alert("please wait your turn");
+    } else {
+      let state = {};
+      state[this.state.grid[cellID]] = "human";
+      this.setState(state, () => console.log(this.state.grid));
+    }
   }
 
   render() {
@@ -35,15 +51,15 @@ class Game extends Component {
       <div className="game">
         <MenueBar />
         <div className="grid">
-          <Cell id="cell_1" onClick={this.handleAction(1)} />
-          <Cell id="cell_2" onClick={this.handleAction(2)} />
-          <Cell id="cell_3" onClick={this.handleAction(3)} />
-          <Cell id="cell_4" onClick={this.handleAction(4)} />
-          <Cell id="cell_5" onClick={this.handleAction(5)} />
-          <Cell id="cell_6" onClick={this.handleAction(6)} />
-          <Cell id="cell_7" onClick={this.handleAction(7)} />
-          <Cell id="cell_8" onClick={this.handleAction(8)} />
-          <Cell id="cell_9" onClick={this.handleAction(9)} />
+          <Cell id="1" onClick={() => this.cellClicked("cell_1")} />
+          <Cell id="2" onClick={() => this.cellClicked("cell_2")} />
+          <Cell id="3" onClick={() => this.cellClicked("cell_3")} />
+          <Cell id="4" onClick={() => this.cellClicked("cell_4")} />
+          <Cell id="5" onClick={() => this.cellClicked("cell_5")} />
+          <Cell id="6" onClick={() => this.cellClicked("cell_6")} />
+          <Cell id="7" onClick={() => this.cellClicked("cell_7")} />
+          <Cell id="8" onClick={() => this.cellClicked("cell_8")} />
+          <Cell id="9" onClick={() => this.cellClicked("cell_9")} />
         </div>
       </div>
     );
--- src/pages/Game/scripts/randBoolean.js
+++ src/pages/Game/scripts/randBoolean.js
@@ -1,5 +1,4 @@
 const randBoolean = () => {
-  console.log(Math.random());
   if (Math.random() > 0.5) return true;
   else return false;
 };
